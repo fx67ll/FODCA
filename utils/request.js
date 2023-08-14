bb32b810eb512bc3848985f4976f4c5055b606c1
fx67ll <2717745014@qq.com>
2023-08-14 19:05:12 +0800

    optimize login relauch

--- utils/request.js
+++ utils/request.js
@@ -1,73 +1,91 @@
-import store from '@/store'
-import config from '@/config'
-import { getToken } from '@/utils/auth'
-import errorCode from '@/utils/errorCode'
-import { toast, showConfirm, tansParams } from '@/utils/common'
+import store from '@/store';
+import config from '@/config';
+import { getToken } from '@/utils/auth';
+import errorCode from '@/utils/errorCode';
+import { toast, showConfirm, tansParams } from '@/utils/common';
 
-let timeout = 10000
-const baseUrl = config.baseUrl
+let timeout = 10000;
+const baseUrl = config.baseUrl;
 
 const request = config => {
   // 是否需要设置 token
-  const isToken = (config.headers || {}).isToken === false
-  config.header = config.header || {}
-  if (getToken() && !isToken) {
-    config.header['Authorization'] = 'Bearer ' + getToken()
+  const isToken = (config.headers || {}).isToken === false;
+  config.header = config.header || {};
+
+  // 如果原来就没有token，直接跳转登录界面
+  // 如果有token，说明之前登陆过，就让后端验证token是否失效
+  if (!getToken() && config?.url !== '/captchaImage') {
+    uni.reLaunch({
+      url: '/pages/login',
+    });
   }
+
+  if (getToken() && !isToken) {
+    config.header['Authorization'] = 'Bearer ' + getToken();
+  }
+
   // get请求映射params参数
   if (config.params) {
-    let url = config.url + '?' + tansParams(config.params)
-    url = url.slice(0, -1)
-    config.url = url
+    let url = config.url + '?' + tansParams(config.params);
+    url = url.slice(0, -1);
+    config.url = url;
   }
+
   return new Promise((resolve, reject) => {
-    uni.request({
+    uni
+      .request({
         method: config.method || 'get',
-        timeout: config.timeout ||  timeout,
+        timeout: config.timeout || timeout,
         url: config.baseUrl || baseUrl + config.url,
         data: config.data,
         header: config.header,
-        dataType: 'json'
-      }).then(response => {
-        let [error, res] = response
+        dataType: 'json',
+      })
+      .then(response => {
+        let [error, res] = response;
         if (error) {
-          toast('后端接口连接异常')
-          reject('后端接口连接异常')
-          return
+          toast('后端接口连接异常');
+          reject('后端接口连接异常');
+          return;
         }
-        const code = res.data.code || 200
-        const msg = errorCode[code] || res.data.msg || errorCode['default']
+        const code = res.data.code || 200;
+        const msg = errorCode[code] || res.data.msg || errorCode['default'];
         if (code === 401) {
-          showConfirm('登录状态已过期，您可以继续留在该页面，或者重新登录?').then(res => {
-            if (res.confirm) {
-              store.dispatch('LogOut').then(res => {
-                uni.reLaunch({ url: '/pages/login' })
-              })
-            }
-          })
-          reject('无效的会话，或者会话已过期，请重新登录。')
+          //   showConfirm('登录状态已过期，您可以继续留在该页面，或者重新登录?').then(res => {
+          //     if (res.confirm) {
+          //       store.dispatch('LogOut').then(res => {
+          //         uni.reLaunch({
+          //           url: '/pages/login',
+          //         });
+          //       });
+          //     }
+          //   });
+          uni.reLaunch({
+            url: '/pages/login',
+          });
+          reject('无效的会话，或者会话已过期，请重新登录。');
         } else if (code === 500) {
-          toast(msg)
-          reject('500')
+          toast(msg);
+          reject('500');
         } else if (code !== 200) {
-          toast(msg)
-          reject(code)
+          toast(msg);
+          reject(code);
         }
-        resolve(res.data)
+        resolve(res.data);
       })
       .catch(error => {
-        let { message } = error
+        let { message } = error;
         if (message === 'Network Error') {
-          message = '后端接口连接异常'
+          message = '后端接口连接异常';
         } else if (message.includes('timeout')) {
-          message = '系统接口请求超时'
+          message = '系统接口请求超时';
         } else if (message.includes('Request failed with status code')) {
-          message = '系统接口' + message.substr(message.length - 3) + '异常'
+          message = '系统接口' + message.substr(message.length - 3) + '异常';
         }
-        toast(message)
-        reject(error)
-      })
-  })
-}
+        toast(message);
+        reject(error);
+      });
+  });
+};
 
-export default request
+export default request;
