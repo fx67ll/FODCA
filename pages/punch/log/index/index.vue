a6decfa8751e2d524946a8e9fc2b074b0f853be1
fx67ll <2717745014@qq.com>
2023-12-06 18:11:27 +0800

    add for test

--- pages/punch/log/index/index.vue
+++ pages/punch/log/index/index.vue
@@ -1,44 +1,37 @@
 <template>
   <view class="fx67ll-extra-box">
-    <z-paging-mini ref="paging" v-model="extraList" @query="queryExtraList">
+    <z-paging-mini ref="paging" v-model="punchList" @query="queryPunchList">
       <uni-swipe-action>
-        <view class="fx67ll-extra-item" v-for="item in extraList" :key="item.extraId">
+        <view class="fx67ll-extra-item" v-for="item in punchList" :key="item.punchId">
           <uni-swipe-action-item
             :right-options="actionOptions"
             @click="(e) => handleActionClick(e, item)"
           >
-            <uni-section :title="item.createTime" type="line">
+            <uni-section :title="item.updateTime.substring(0, 10)" type="line">
               <uni-list :border="true">
                 <uni-list-chat
                   :avatar="
                     'https://vip.fx67ll.com/vip-api/getRandomAvatar?avatarBlockNum=' + 6
                   "
-                  :title="`￥${item.extraMoney}${
-                    item.saveMoney && item.saveMoney !== '0' ? ' / ￥' + item.saveMoney : ''
-                  }`"
-                  :note="item.extraRemark"
+                  :title="item.updateTime"
+                  :note="item.punchRemark || '暂无打卡备注'"
                 >
                   <template v-slot:default>
-                    <view
-                      :style="{ fontSize: '14px', color: '#999', paddingRight: '6rpx' }"
-                      >￥{{ item.seedMoney }}
-                    </view>
                     <view>
                       <text
                         :class="{
                           'fx67ll-extra-badge': true,
-                          'fx67ll-extra-badge-success': item.isWin === 'Y',
-                          'fx67ll-extra-badge-fail':
-                            item.isWin !== 'Y' && item.winMoney !== '0',
+                          'fx67ll-extra-badge-success': item.punchType === '1',
+                          'fx67ll-extra-badge-fail': item.punchType === '2',
                           'fx67ll-extra-badge-normal':
-                            item.isWin !== 'Y' && item.winMoney === '0',
+                            !item.punchType || item.punchType === '3',
                         }"
                         >{{
-                          item.isWin === "Y"
-                            ? `+${item.winMoney}`
-                            : item.winMoney === "0"
-                            ? "0"
-                            : `${item.winMoney}`
+                          item.punchType === "1"
+                            ? "上班打卡"
+                            : item.punchType === "2"
+                            ? "下班打卡"
+                            : "异常记录，请联系管理员"
                         }}
                       </text>
                     </view>
@@ -50,33 +43,36 @@
         </view>
       </uni-swipe-action>
     </z-paging-mini>
-    <uni-fab
-      ref="fab"
-      :pattern="fabConfig.pattern"
-      :content="fabConfig.content"
-      :horizontal="fabConfig.horizontal"
-      :vertical="fabConfig.vertical"
-      :direction="fabConfig.direction"
-      @trigger="handleFabTrigger"
-    />
   </view>
 </template>
 
 <script>
-import { getExtraList, delExtra } from "@/api/dortmund/extra";
+import { getPunchLogList, delPunchLog } from "@/api/punch/log";
+
 import { diffTimeStrFromNow } from "@/utils/index";
 import { showConfirm } from "@/utils/common";
+
 import uniListChat from "@/uni_modules/uni-list/components/uni-list-chat/uni-list-chat.vue";
+
+import moment from "@/node_modules/moment";
+import "@/node_modules/moment/locale/zh-cn";
+
 export default {
   components: { uniListChat },
   data() {
     return {
-      extraList: [],
+      punchList: [],
       actionOptions: [
         {
           text: "取消",
           style: {
             backgroundColor: "#4096ff",
+          },
+        },
+        {
+          text: "修改",
+          style: {
+            backgroundColor: "#2ecc71",
           },
         },
         {
@@ -86,65 +82,59 @@ export default {
           },
         },
       ],
-      fabConfig: {
-        title: "uni-fab",
-        directionStr: "垂直",
-        horizontal: "right",
-        vertical: "bottom",
-        direction: "horizontal",
-        pattern: {
-          color: "#7A7E83",
-          backgroundColor: "#ffffff",
-          selectedColor: "#7A7E83",
-          buttonColor: "#2ecc71",
-          iconColor: "#ffffff",
-        },
-        is_color_type: false,
-        content: [
-          {
-            iconPath:
-              "https://vip.fx67ll.com/vip-api/getRandomAvatar?avatarBlockNum=5&avatarPadding=30",
-            selectedIconPath:
-              "https://vip.fx67ll.com/vip-api/getRandomAvatar?avatarBlockNum=5&avatarPadding=30",
-            text: "新增",
-            active: false,
-          },
-          {
-            iconPath:
-              "https://vip.fx67ll.com/vip-api/getRandomAvatar?avatarBlockNum=5&avatarPadding=29",
-            selectedIconPath:
-              "https://vip.fx67ll.com/vip-api/getRandomAvatar?avatarBlockNum=5&avatarPadding=29",
-            text: "走势",
-            active: false,
-          },
-        ],
-      },
     };
   },
-  onBackPress() {
-    if (this.$refs.fab.isShow) {
-      this.$refs.fab.close();
-      return true;
-    }
-    return false;
-  },
   methods: {
-    handleFabTrigger(e) {
-      if (e?.index === 0) {
-        this.$tab.navigateTo("/pages/dortmund/extra/add/add");
-      }
-      if (e?.index === 1) {
-        this.$tab.navigateTo("/pages/dortmund/extra/chart/chart");
-      }
-      this.$refs.fab.close();
-    },
-    queryExtraList(pageNum, pageSize) {
+    // formatDataByDate() {
+    //   // 创建一个空对象来存储聚合后的数据
+    //   const aggregatedData = {};
+    //   // 遍历原始数据
+    //   originalData.forEach((item) => {
+    //     // 提取日期部分
+    //     const date = item.updateTime.substring(0, 10);
+    //     // 如果在聚合数据中不存在以该日期为键的对象，则创建一个空数组
+    //     if (!aggregatedData[date]) {
+    //       aggregatedData[date] = [];
+    //     }
+    //     // 将当前数据项添加到以该日期为键的数组中
+    //     aggregatedData[date].push(item);
+    //   });
+
+    //   // 转换聚合数据对象为数组
+    //   const result = Object.keys(aggregatedData).map((date) => ({
+    //     date,
+    //     data: aggregatedData[date],
+    //   }));
+    //   // 打印看下数据
+    //   console.log(result);
+    // },
+    queryPunchList(pageNum, pageSize) {
       const self = this;
+
+      // const beginDayIndex = pageNum * pageSize + 1;
+      // const endDayIndex = (pageNum - 1) * pageSize - 1;
+      // const beginUpdateTime = moment().subtract(beginDayIndex, "days").format("YYYY-MM-DD");
+      // const endUpdateTime = moment()
+      //   .subtract(endDayIndex, "days")
+      //   .format("YYYY-MM-DD");
+
       const queryParams = {
         pageNum,
         pageSize,
+        // beginUpdateTime,
+        // endUpdateTime,
       };
-      getExtraList(queryParams)
+
+      // console.log(
+      //   pageNum,
+      //   pageSize,
+      //   beginDayIndex,
+      //   endDayIndex,
+      //   beginUpdateTime,
+      //   endUpdateTime
+      // );
+
+      getPunchLogList(queryParams)
         .then((res) => {
           if (res?.code === 200) {
             if (res?.rows && res?.rows?.length > 0) {
@@ -154,7 +144,7 @@ export default {
             }
           } else {
             uni.showToast({
-              title: "查询外快盈亏记录失败！",
+              title: "查询历史打卡记录失败！",
               icon: "none",
               duration: 1998,
             });
@@ -165,20 +155,24 @@ export default {
           self.$refs.paging.complete(false);
         });
     },
-    deleteExtra(id) {
+    // 修改历史打卡记录详情
+    editLogInfo(punchId) {
+      // this.$tab.navigateTo(`/pages/punch/log/edit/edit?lotteryId=${punchId}`);
+    },
+    deletePunch(punchId) {
       const self = this;
-      delExtra(id).then((res) => {
+      delPunchLog(punchId).then((res) => {
         if (res?.code === 200) {
-          // self.queryExtraList();
+          // self.queryPunchList();
           self.$refs.paging.reload();
           uni.showToast({
-            title: "外快盈亏记录删除成功！",
+            title: "历史打卡记录删除成功！",
             icon: "none",
             duration: 1998,
           });
         } else {
           uni.showToast({
-            title: "外快盈亏记录删除失败！",
+            title: "历史打卡记录删除失败！",
             icon: "none",
             duration: 1998,
           });
@@ -188,19 +182,27 @@ export default {
     handleActionClick(e, record) {
       const self = this;
       if (e?.index === 1) {
+        // this.editPunchLogInfo(record?.punchId);
+        uni.showToast({
+          title: "功能开发中！",
+          icon: "none",
+          duration: 1998,
+        });
+      }
+      if (e?.index === 2) {
         showConfirm(
-          `删除后数据无法恢复，请确认是否删除创建时间为：${record?.createTime} 的外快盈亏记录？`,
+          `删除后数据无法恢复，请确认是否删除创建时间为：${record?.createTime} 的历史打卡记录？`,
           "警告"
         ).then((res) => {
-          if (res?.confirm && record?.extraId) {
-            if (diffTimeStrFromNow(record?.createTime) <= -24) {
+          if (res?.confirm && record?.punchId) {
+            if (diffTimeStrFromNow(record?.createTime) <= -48) {
               uni.showToast({
-                title: "记录创建时间已超过24小时，不允许删除操作！",
+                title: "记录创建时间已超过48小时，不允许删除操作！有需要请联系管理员~",
                 icon: "none",
                 duration: 1998,
               });
             } else {
-              self.deleteExtra(record.extraId);
+              self.deletePunch(record.punchId);
             }
           }
         });
