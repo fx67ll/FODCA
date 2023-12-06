483036e0d152f2a2668b3f451145e25dcefe3691
fx67ll <2717745014@qq.com>
2023-12-06 18:11:41 +0800

    add api file

---
+++ api/punch/log.js
@@ -0,0 +1,44 @@
+import request from "@/utils/request";
+
+// 查询打卡记录列表
+export function getPunchLogList(query) {
+  return request({
+    url: "/punch/log/getPunchLogListForApp",
+    method: "get",
+    params: query,
+  });
+}
+
+// 查询打卡记录详细
+export function getPunchLogInfo(punchId) {
+  return request({
+    url: "/punch/log/getPunchLogInfoForApp/" + punchId,
+    method: "get",
+  });
+}
+
+// 新增打卡记录
+export function addPunchLog(data) {
+  return request({
+    url: "/punch/log/addPunchLogForApp",
+    method: "post",
+    data: data,
+  });
+}
+
+// 修改打卡记录
+export function updatePunchLog(data) {
+  return request({
+    url: "/punch/log/editPunchLogForApp",
+    method: "put",
+    data: data,
+  });
+}
+
+// 删除打卡记录
+export function delPunchLog(punchId) {
+  return request({
+    url: "/punch/log/deletePunchLogByIdForApp/" + punchId,
+    method: "delete",
+  });
+}
