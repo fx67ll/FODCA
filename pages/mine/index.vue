13b003732ee62e5165e4d3f52ceec2154ee8f6ae
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2024-05-20 17:27:04 +0800

    add new entrance

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -56,6 +56,12 @@
             <view>历史号码记录</view>
           </view>
         </view>
+        <view class="list-cell list-cell-arrow" @click="handleToLogStatistics">
+          <view class="menu-item-box">
+            <view><uni-icons color="#2ecc71" type="flag" size="20"></uni-icons></view>
+            <view>中奖金额统计</view>
+          </view>
+        </view>
         <!-- 只允许超级管理员fx67ll使用外快盈亏管理的功能，后续视情况开放 -->
         <!-- 2023已禁用，2024再出发 -->
         <view
@@ -71,7 +77,7 @@
         <view class="list-cell list-cell-arrow" @click="handleToPunchLog">
           <view class="menu-item-box">
             <view><uni-icons color="#2ecc71" type="calendar" size="20"></uni-icons></view>
-            <view>打卡工时统计</view>
+            <view>打卡工时管理</view>
           </view>
         </view>
         <!-- <view class="list-cell list-cell-arrow" @click="handleToEditInfo">
@@ -133,6 +139,9 @@ export default {
     },
     handleToLogHistory() {
       this.$tab.navigateTo("/pages/lottery/log/index/index");
+    },
+    handleToLogStatistics() {
+      this.$tab.navigateTo("/pages/lottery/statistics/statistics");
     },
     handleToExtraManagement() {
       this.$tab.navigateTo("/pages/dortmund/extra/index/index");
