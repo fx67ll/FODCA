0f6dcd865fc698c2b9aa013680fe4a0b04e26f57
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2024-06-25 09:58:22 +0800

    new version for H5 App

--- utils/index.js
+++ utils/index.js
@@ -130,7 +130,7 @@ export function checkLotteryResult(lotteryType, recordNumStr, winNumStr) {
   const winningFrontNumbers = winNumStr.split('-')[0].split(',');
   const winningBackNumbers = winNumStr.split('-')[1].split(',');
 
-  console.log(lotteryType, frontNumbers, backNumbers, winningFrontNumbers, winningBackNumbers);
+  // console.log(lotteryType, frontNumbers, backNumbers, winningFrontNumbers, winningBackNumbers);
 
   const result = {
     prizeLevel: 0,
@@ -274,7 +274,7 @@ export function checkLotteryResult(lotteryType, recordNumStr, winNumStr) {
     }
   }
 
-  console.log(result);
+  // console.log(result);
   return result;
 }
 
