7ee71df5a8e9d648c611a53f3a89d23058a658bd
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2025-08-05 17:00:12 +0800

    add cube game

--- pages/mine/index.vue
+++ pages/mine/index.vue
@@ -7,13 +7,7 @@
           <view v-if="!avatar" class="cu-avatar xl round bg-white">
             <view class="iconfont icon-people text-gray icon"></view>
           </view>
-          <image
-            v-if="avatar"
-            @click="handleToAvatar"
-            :src="avatar"
-            class="cu-avatar xl round"
-            mode="widthFix"
-          >
+          <image v-if="avatar" @click="handleToAvatar" :src="avatar" class="cu-avatar xl round" mode="widthFix">
           </image>
           <view v-if="!userName" @click="handleToLogin" class="login-tip">
             点击登录
@@ -64,20 +58,26 @@
         </view>
         <!-- 只允许超级管理员fx67ll使用外快盈亏管理的功能，后续视情况开放 -->
         <!-- 2023已禁用，2024再出发 -->
-        <!-- <view
-          class="list-cell list-cell-arrow"
-          @click="handleToExtraManagement"
-          v-if="userName && userName === 'fx67ll' && isNeedWaiKuai"
-        >
+        <view class="list-cell list-cell-arrow" @click="handleToExtraManagement"
+          v-if="userName && userName === 'fx67ll' && isNeedWaiKuai">
           <view class="menu-item-box">
             <view><uni-icons color="#2ecc71" type="vip" size="20"></uni-icons></view>
             <view>外快盈亏管理</view>
           </view>
-        </view> -->
+        </view>
         <view class="list-cell list-cell-arrow" @click="handleToPunchLog">
           <view class="menu-item-box">
             <view><uni-icons color="#2ecc71" type="calendar" size="20"></uni-icons></view>
             <view>打卡工时管理</view>
+          </view>
+        </view>
+      </view>
+
+      <view class="menu-list menu-list-last">
+        <view class="list-cell list-cell-arrow" @click="handleToCubeGame">
+          <view class="menu-item-box">
+            <view><uni-icons color="#2ecc71" type="star" size="20"></uni-icons></view>
+            <view>魔方小游戏</view>
           </view>
         </view>
         <!-- <view class="list-cell list-cell-arrow" @click="handleToEditInfo">
@@ -88,9 +88,7 @@
         </view> -->
         <view class="list-cell list-cell-arrow" @click="handleAbout">
           <view class="menu-item-box">
-            <view
-              ><uni-icons color="#2ecc71" type="paperclip" size="20"></uni-icons
-            ></view>
+            <view><uni-icons color="#2ecc71" type="paperclip" size="20"></uni-icons></view>
             <view>关于我们</view>
           </view>
         </view>
@@ -106,163 +104,172 @@
 </template>
 
 <script>
-// 获取加密配置
-import { getSecretConfig } from "@/api/secret/key.js";
-import { decryptString } from "@/utils/index";
-import { getCryptoSaltKey } from "@/neverUploadToGithub";
+  // 获取加密配置
+  import { getSecretConfig } from "@/api/secret/key.js";
+  import { decryptString } from "@/utils/index";
+  import { getCryptoSaltKey } from "@/neverUploadToGithub";
 
-export default {
-  data() {
-    return {
-      // 当前登录用户信息
-      userName: this.$store.state.user.name,
-      version: getApp().globalData.config.appInfo.version,
-      globalConfig: getApp().globalData.config,
-      // 是否需要外快的配置项
-      isNeedWaiKuai: uni.getStorageSync("isNeedWaiKuai") || false,
-    };
-  },
-  computed: {
-    avatar() {
-      return this.$store.state.user.avatar;
+  export default {
+    data() {
+      return {
+        // 当前登录用户信息
+        userName: this.$store.state.user.name,
+        version: getApp().globalData.config.appInfo.version,
+        globalConfig: getApp().globalData.config,
+        // 是否需要外快的配置项
+        isNeedWaiKuai: uni.getStorageSync("isNeedWaiKuai") || false,
+      };
     },
-    windowHeight() {
-      return uni.getSystemInfoSync().windowHeight - 50;
+    computed: {
+      avatar() {
+        return this.$store.state.user.avatar;
+      },
+      windowHeight() {
+        return uni.getSystemInfoSync().windowHeight - 50;
+      },
     },
-  },
-  onLoad() {
-    this.getWKConfig();
-  },
-  onShow() {
-    console.log(
-      'uni.getStorageSync("isNeedWaiKuai")',
-      uni.getStorageSync("isNeedWaiKuai")
-    );
-    this.isNeedWaiKuai = uni.getStorageSync("isNeedWaiKuai");
-  },
-  methods: {
-    handleToInfo() {
-      this.$tab.navigateTo("/pages/mine/info/index");
+    onLoad() {
+      this.getWKConfig();
     },
-    handleToLogHistory() {
-      this.$tab.navigateTo("/pages/lottery/log/index/index");
-    },
-    handleToLogStatistics() {
-      this.$tab.navigateTo("/pages/lottery/statistics/statistics");
-    },
-    handleToExtraManagement() {
-      this.$tab.navigateTo("/pages/dortmund/extra/index/index");
-    },
-    handleToPunchLog() {
-      this.$tab.navigateTo("/pages/punch/log/record/record");
-    },
-    // handleToEditInfo() {
-    //   this.$tab.navigateTo("/pages/mine/info/edit");
-    // },
-    handleToSetting() {
-      this.$tab.navigateTo("/pages/mine/setting/index");
-    },
-    handleToLogin() {
-      this.$tab.reLaunch("/pages/login");
-    },
-    handleToAvatar() {
-      this.$tab.navigateTo("/pages/mine/avatar/index");
-    },
-    handleLogout() {
-      this.$modal.confirm("确定注销并退出系统吗？").then(() => {
-        this.$store.dispatch("LogOut").then(() => {
-          this.$tab.reLaunch("/pages/index");
-        });
-      });
-    },
-    handleAbout() {
-      this.$tab.navigateTo("/pages/mine/about/index");
-    },
-    goFx67ll(index) {
-      let site = this.globalConfig.appInfo.author_info[index];
-      this.$tab.navigateTo(
-        `/pages/common/webview/index?title=${site.title}&url=${site.url}`
+    onShow() {
+      console.log(
+        'uni.getStorageSync("isNeedWaiKuai")',
+        uni.getStorageSync("isNeedWaiKuai")
       );
+      this.isNeedWaiKuai = uni.getStorageSync("isNeedWaiKuai");
     },
-    getWKConfig() {
-      const self = this;
-      getSecretConfig({ secretKey: "isNeedWaiKuai" }).then((res) => {
-        if (res && res?.rows && res?.rows.length > 0 && res?.code === 200) {
-          const wkTmp = JSON.parse(
-            decryptString(res.rows[0].secretValue, getCryptoSaltKey()) || "false"
-          );
-          uni.setStorageSync("isNeedWaiKuai", wkTmp);
-          self.isNeedWaiKuai = wkTmp;
-        }
-      });
+    methods: {
+      handleToInfo() {
+        this.$tab.navigateTo("/pages/mine/info/index");
+      },
+      handleToLogHistory() {
+        this.$tab.navigateTo("/pages/lottery/log/index/index");
+      },
+      handleToLogStatistics() {
+        this.$tab.navigateTo("/pages/lottery/statistics/statistics");
+      },
+      handleToExtraManagement() {
+        this.$tab.navigateTo("/pages/dortmund/extra/index/index");
+      },
+      handleToPunchLog() {
+        this.$tab.navigateTo("/pages/punch/log/record/record");
+      },
+      handleToCubeGame() {
+        this.$tab.navigateTo(
+          `/pages/common/webview/index?title=魔方小游戏&url=https://three.fx67ll.com/cube`
+        );
+      },
+      // handleToEditInfo() {
+      //   this.$tab.navigateTo("/pages/mine/info/edit");
+      // },
+      handleToSetting() {
+        this.$tab.navigateTo("/pages/mine/setting/index");
+      },
+      handleToLogin() {
+        this.$tab.reLaunch("/pages/login");
+      },
+      handleToAvatar() {
+        this.$tab.navigateTo("/pages/mine/avatar/index");
+      },
+      handleLogout() {
+        this.$modal.confirm("确定注销并退出系统吗？").then(() => {
+          this.$store.dispatch("LogOut").then(() => {
+            this.$tab.reLaunch("/pages/index");
+          });
+        });
+      },
+      handleAbout() {
+        this.$tab.navigateTo("/pages/mine/about/index");
+      },
+      goFx67ll(index) {
+        let site = this.globalConfig.appInfo.author_info[index];
+        this.$tab.navigateTo(
+          `/pages/common/webview/index?title=${site.title}&url=${site.url}`
+        );
+      },
+      getWKConfig() {
+        const self = this;
+        getSecretConfig({ secretKey: "isNeedWaiKuai" }).then((res) => {
+          if (res && res?.rows && res?.rows.length > 0 && res?.code === 200) {
+            const wkTmp = JSON.parse(
+              decryptString(res.rows[0].secretValue, getCryptoSaltKey()) || "false"
+            );
+            uni.setStorageSync("isNeedWaiKuai", wkTmp);
+            self.isNeedWaiKuai = wkTmp;
+          }
+        });
+      },
     },
-  },
-};
+  };
 </script>
 
 <style lang="scss">
-page {
-  background-color: #f5f6f7;
-}
+  page {
+    background-color: #f5f6f7;
+  }
 
-.mine-container {
-  width: 100%;
-  height: 100%;
+  .mine-container {
+    width: 100%;
+    height: 100%;
 
-  .header-section {
-    padding: 15px 15px 45px 15px;
-    background-color: #2ecc71;
-    color: white;
+    .header-section {
+      padding: 15px 15px 45px 15px;
+      background-color: #2ecc71;
+      color: white;
 
-    .login-tip {
-      font-size: 18px;
-      margin-left: 10px;
-    }
-
-    .cu-avatar {
-      border: 2px solid #eaeaea;
-
-      .icon {
-        font-size: 40px;
-      }
-    }
-
-    .user-info {
-      margin-left: 15px;
-
-      .u_title {
+      .login-tip {
         font-size: 18px;
-        line-height: 30px;
+        margin-left: 10px;
       }
-    }
-  }
 
-  .content-section {
-    position: relative;
-    top: -50px;
+      .cu-avatar {
+        border: 2px solid #eaeaea;
 
-    .menu-list {
-      padding-bottom: 68px;
-    }
-
-    .mine-actions {
-      margin: 15px 15px;
-      padding: 20px 0px;
-      border-radius: 8px;
-      background-color: white;
-
-      .action-item {
         .icon {
-          font-size: 28px;
+          font-size: 40px;
         }
+      }
 
-        .text {
-          display: block;
-          font-size: 13px;
-          margin: 8px 0px;
+      .user-info {
+        margin-left: 15px;
+
+        .u_title {
+          font-size: 18px;
+          line-height: 30px;
+        }
+      }
+    }
+
+    .content-section {
+      position: relative;
+      top: -50px;
+
+      .menu-list {
+        margin-top: 15px;
+      }
+
+      .menu-list-last {
+        padding-bottom: 68px;
+      }
+
+      .mine-actions {
+        margin: 15px 15px 0 15px;
+        padding: 20px 0px;
+        border-radius: 8px;
+        background-color: white;
+
+        .action-item {
+          .icon {
+            font-size: 28px;
+          }
+
+          .text {
+            display: block;
+            font-size: 13px;
+            margin: 8px 0px;
+          }
         }
       }
     }
   }
-}
 </style>
