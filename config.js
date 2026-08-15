d0e309379d7bb70c41966cf0c0a82273e7ec272f
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-08-15 18:40:06 +0800

    new version record

--- config.js
+++ config.js
@@ -3,13 +3,23 @@ module.exports = {
   baseUrl: 'https://vip.fx67ll.com/vip-api',
   // baseUrl: 'http://localhost:9897/vip-api',
   // 不校验登录信息的api
-  white_api: ['/captchaImage', '/login', '/secret/key/getSecretKeyConfigForApp', '/public/status/overview', '/auth/app/loginToken', '/auth/app/oneClickLogin', '/auth/app/checkLoginEligibility', '/getRandomAvatar', '/getRandomAvatarByBase64'],
+  white_api: [
+    '/captchaImage',
+    '/login',
+    '/secret/key/getSecretKeyConfigForApp',
+    '/public/status/overview',
+    '/auth/app/loginToken',
+    '/auth/app/oneClickLogin',
+    '/auth/app/checkLoginEligibility',
+    '/getRandomAvatar',
+    '/getRandomAvatarByBase64',
+  ],
   // 应用信息
   appInfo: {
     // 应用名称
     name: 'fx67ll-app',
     // 应用版本
-    version: '0.8.11',
+    version: '0.8.12',
     // 应用logo
     logo: 'https://test.fx67ll.com/fx67ll-img-collection/fx67ll.jpg',
     // 个人主页
